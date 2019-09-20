4518703313a723f741f96ad78d942213e860b44c
T-Lone <valtrinty@orange.fr>
2019-09-20 08:36:06 +0200

    test3

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-# This file is used by the build system to adjust CSS and JS output to support the specified browsers below.
-# For additional information regarding the format and rule options, please see:
-# https://github.com/browserslist/browserslist#queries
+// server.js
 
-# You can see what browsers were selected by your queries by running:
-#   npx browserslist
+const express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    mongoose = require('mongoose');
 
-> 0.5%
-last 2 versions
-Firefox ESR
-not dead
-not IE 9-11 # For IE 9-11 support, remove 'not'.
+    const app = express();
+    let port = process.env.PORT || 4000;
+
+    const server = app.listen(function(){
+        console.log('Listening on port ' + port);
+    });
+    
